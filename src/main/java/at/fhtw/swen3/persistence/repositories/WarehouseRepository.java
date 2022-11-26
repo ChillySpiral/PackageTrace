be2255b19fa041f40f45c38b0f1a2bfbecaa45ae
ChillySpiral <81975828+ChillySpiral@users.noreply.github.com>
2022-11-26 11:41:23 +0100

    Added Repository Test classes

--- src/main/java/at/fhtw/swen3/persistence/repositories/WarehouseRepository.java
+++ src/main/java/at/fhtw/swen3/persistence/repositories/WarehouseRepository.java
@@ -3,10 +3,11 @@ package at.fhtw.swen3.persistence.repositories;
 import at.fhtw.swen3.persistence.entities.WarehouseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface WarehouseRepository extends JpaRepository<WarehouseEntity, Long> {
 
-    WarehouseEntity findAllByLevel(Integer level);
+    List<WarehouseEntity> findAllByLevel(Integer level);
 
 }
---
+++ src/test/java/at/fhtw/swen3/persistence/repositories/HopRepositoryTest.java
@@ -0,0 +1,12 @@
+package at.fhtw.swen3.persistence.repositories;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestPropertySource("/application-test.properties")
+class HopRepositoryTest {
+
+}
